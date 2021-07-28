021d292c1de154cc82795c16ca766c96421c517b
lyandeveloper <tutors3d@gmail.com>
2021-07-28 07:49:45 -0300

    feat: Add add transaction usecase

---
+++ src/domain/usecases/AddTransaction.ts
@@ -0,0 +1,21 @@
+import { Billing } from '../entities/Billing';
+import { CreditCard } from '../entities/CreditCard';
+import { Customer } from '../entities/Customer';
+import { Item } from '../entities/Item';
+import { Shipping } from '../entities/Shipping';
+
+export interface AddTransactionModel {
+  amount: number;
+  card_number: string;
+  card_cvv: string;
+  card_expiration_date: string;
+  card_holder_name: string;
+  customer: Customer;
+  billing: Billing;
+  shipping: Shipping;
+  item: Item[];
+}
+
+export interface AddTransaction {
+  add(transaction: AddTransactionModel): Promise<CreditCard>;
+}
