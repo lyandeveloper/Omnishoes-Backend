79f66d09d411c6cd6e5a0736a7003ab63bb36152
lyandeveloper <tutors3d@gmail.com>
2021-08-03 14:20:24 -0300

    feat: Should return a error if amount field is missing

---
+++ src/presentation/controllers/transaction/add-transaction.ts
@@ -0,0 +1,30 @@
+import { MissingParamError } from '../../errors';
+import { badRequest } from '../../helper/http-helper';
+import { Controller } from '../../protocols/controller';
+import { HttpRequest, HttpResponse } from '../../protocols/http';
+
+export class AddTransactionController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = [
+        'ammount',
+        'card_number',
+        'card_cvv',
+        'card_expiration_date',
+        'card_holder_name',
+        'customer',
+        'billing',
+        'shipping',
+        'items',
+      ];
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
