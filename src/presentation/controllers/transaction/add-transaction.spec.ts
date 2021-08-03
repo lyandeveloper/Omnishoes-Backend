7cb0a1033206a2b96a87a306ae777134074a0150
lyandeveloper <tutors3d@gmail.com>
2021-08-03 14:36:25 -0300

    test: Should return a error if card_expiration_date field is missing

--- src/presentation/controllers/transaction/add-transaction.spec.ts
+++ src/presentation/controllers/transaction/add-transaction.spec.ts
@@ -75,7 +75,7 @@ describe('Add Transaction', () => {
     expect(httpResponse).toEqual(badRequest(new MissingParamError('amount')));
   });
 
-  test('Should return a error if card number field is missing', async () => {
+  test('Should return a error if card_number field is missing', async () => {
     const sut = new AddTransactionController();
     const httpRequest = {
       body: {
@@ -147,7 +147,7 @@ describe('Add Transaction', () => {
     expect(httpResponse).toEqual(badRequest(new MissingParamError('card_number')));
   });
 
-  test('Should return a error if card cvv field is missing', async () => {
+  test('Should return a error if card_cvv field is missing', async () => {
     const sut = new AddTransactionController();
     const httpRequest = {
       body: {
@@ -217,5 +217,77 @@ describe('Add Transaction', () => {
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('card_cvv')));
+  });
+
+  test('Should return a error if card_expiration_date field is missing', async () => {
+    const sut = new AddTransactionController();
+    const httpRequest = {
+      body: {
+        amount: 21000,
+        card_number: '4111111111111111',
+        card_cvv: '123',
+        card_holder_name: 'Morpheus Fishburne',
+        customer: {
+          external_id: '#3311',
+          name: 'Morpheus Fishburne',
+          type: 'individual',
+          country: 'br',
+          email: 'mopheus@nabucodonozor.com',
+          documents: [
+            {
+              type: 'cpf',
+              number: '30621143049',
+            },
+          ],
+          phone_numbers: ['+5511999998888', '+5511888889999'],
+          birthday: '1965-01-01',
+        },
+        billing: {
+          name: 'Trinity Moss',
+          address: {
+            country: 'br',
+            state: 'sp',
+            city: 'Cotia',
+            neighborhood: 'Rio Cotia',
+            street: 'Rua Matrix',
+            street_number: '9999',
+            zipcode: '06714360',
+          },
+        },
+        shipping: {
+          name: 'Neo Reeves',
+          fee: 1000,
+          delivery_date: '2000-12-21',
+          expedited: true,
+          address: {
+            country: 'br',
+            state: 'sp',
+            city: 'Cotia',
+            neighborhood: 'Rio Cotia',
+            street: 'Rua Matrix',
+            street_number: '9999',
+            zipcode: '06714360',
+          },
+        },
+        items: [
+          {
+            id: 'r123',
+            title: 'Red pill',
+            unit_price: 10000,
+            quantity: 1,
+            tangible: true,
+          },
+          {
+            id: 'b123',
+            title: 'Blue pill',
+            unit_price: 10000,
+            quantity: 1,
+            tangible: true,
+          },
+        ],
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('card_expiration_date')));
   });
 });
