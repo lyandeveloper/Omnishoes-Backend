0db6fe88732912bed52885dd225af1d0447abd61
lyandeveloper <tutors3d@gmail.com>
2021-08-04 17:35:51 -0300

    feat: Should return 500 if AddTransaction throws

--- src/presentation/controllers/transaction/add-transaction.ts
+++ src/presentation/controllers/transaction/add-transaction.ts
@@ -1,9 +1,17 @@
+import { AddTransaction } from '../../../domain/usecases/AddTransaction';
 import { MissingParamError } from '../../errors';
-import { badRequest } from '../../helper/http-helper';
+import { ServerError } from '../../errors/server-error';
+import { badRequest, serverError } from '../../helper/http-helper';
 import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 
 export class AddTransactionController implements Controller {
+  private readonly addTransaction: AddTransaction;
+
+  constructor(addTransaction: AddTransaction) {
+    this.addTransaction = addTransaction;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = [
@@ -23,8 +31,32 @@ export class AddTransactionController implements Controller {
           return badRequest(new MissingParamError(field));
         }
       }
+
+      const {
+        amount,
+        card_number,
+        card_cvv,
+        card_expiration_date,
+        card_holder_name,
+        customer,
+        billing,
+        shipping,
+        item,
+      } = httpRequest.body;
+
+      await this.addTransaction.add({
+        amount,
+        card_number,
+        card_cvv,
+        card_expiration_date,
+        card_holder_name,
+        customer,
+        billing,
+        shipping,
+        item,
+      });
     } catch (error) {
-      console.log(error);
+      return serverError(new ServerError());
     }
   }
 }
