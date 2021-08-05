e6afc5c1d6871981f7146a797e56c6068bf14532
lyandeveloper <tutors3d@gmail.com>
2021-08-05 14:24:16 -0300

    feat: Content Type Middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,8 @@
 import { Express } from 'express';
-import { bodyParser } from '../middlewares';
+import { bodyParser, contentType } from '../middlewares';
 
 export default (app: Express): void => {
   app.use(bodyParser);
-//   app.use(cors);
-//   app.use(contentType);
+  //   app.use(cors);
+  app.use(contentType);
 };
